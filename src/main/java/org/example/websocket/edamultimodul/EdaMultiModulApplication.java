d1b4cc8a9af87137df7962dd45689112e091b12f
Developer-Choi-Jae-Young <77819208+Developer-Choi-Jae-Young@users.noreply.github.com>
2026-01-25 13:05:01 +0900

    init project

---
+++ src/main/java/org/example/websocket/edamultimodul/EdaMultiModulApplication.java
@@ -0,0 +1,13 @@
+package org.example.websocket.edamultimodul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdaMultiModulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdaMultiModulApplication.class, args);
+    }
+
+}
