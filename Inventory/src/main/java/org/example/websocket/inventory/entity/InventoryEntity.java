63797eb1f241913a3540d90f3aaace986d4bb40f
Developer-Choi-Jae-Young <77819208+Developer-Choi-Jae-Young@users.noreply.github.com>
2026-01-28 23:34:31 +0900

    비관적락 !!!

--- Inventory/src/main/java/org/example/websocket/inventory/entity/InventoryEntity.java
+++ Inventory/src/main/java/org/example/websocket/inventory/entity/InventoryEntity.java
@@ -20,9 +20,6 @@ public class InventoryEntity {
     @Column(name = "productId")
     private Long product;
 
-    @Version  // <- 버전 컬럼 추가
-    private Long version;
-
     public void decrementInventory(int quantity) {
         this.quantity-= quantity;
     }
--- Inventory/src/main/java/org/example/websocket/inventory/repository/InventoryRepository.java
+++ Inventory/src/main/java/org/example/websocket/inventory/repository/InventoryRepository.java
@@ -1,13 +1,15 @@
 package org.example.websocket.inventory.repository;
 
+import jakarta.persistence.LockModeType;
 import org.example.websocket.inventory.entity.InventoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface InventoryRepository extends JpaRepository<InventoryEntity,Long> {
-//    @Lock(LockModeType.PESSIMISTIC_WRITE)  // <- 비관적 쓰기 락
+    @Lock(LockModeType.PESSIMISTIC_WRITE)  // <- 비관적 쓰기 락
     Optional<InventoryEntity> findByProduct(Long productId);
 }
