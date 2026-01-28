aebdf622dd1e559ad79fbb014645bf160383e2fe
Developer-Choi-Jae-Young <77819208+Developer-Choi-Jae-Young@users.noreply.github.com>
2026-01-28 23:33:18 +0900

    낙관적락 !!!

--- Inventory/src/main/java/org/example/websocket/inventory/entity/InventoryEntity.java
+++ Inventory/src/main/java/org/example/websocket/inventory/entity/InventoryEntity.java
@@ -20,8 +20,8 @@ public class InventoryEntity {
     @Column(name = "productId")
     private Long product;
 
-//    @Version  // <- 버전 컬럼 추가
-//    private Long version;
+    @Version  // <- 버전 컬럼 추가
+    private Long version;
 
     public void decrementInventory(int quantity) {
         this.quantity-= quantity;
